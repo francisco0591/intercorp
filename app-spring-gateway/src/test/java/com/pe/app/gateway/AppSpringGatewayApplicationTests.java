1edb6c06964178c61f3aa1085c90f76f82f53df7
fjulcav <fjulcav@gmail.com>
2024-09-17 16:31:18 -0500

    cambios api gateway

---
+++ app-spring-gateway/src/test/java/com/pe/app/gateway/AppSpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
